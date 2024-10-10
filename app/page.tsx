75985ee9f29425c7691187d8eaefdddcade11c02
Michael Dreesen <michaeldreesen90@gmail.com>
2024-10-10 15:02:30 -0600

    feature(Adding header and offer sections)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
 
+// Import components
+import Hero from '@/components/Hero';
+import WhatWeOffer from "@/components/WhatWeOffer";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     
+    <div className="items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <section>
+        <Hero/>
+      </section>
+      <section>
+        <WhatWeOffer/>
+      </section>
     </div>
   );
 }
